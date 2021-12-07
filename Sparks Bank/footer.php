1afe52d956df202cc11273895f45c5480ab61c5c
Atul Patil <58878642+priyesh069@users.noreply.github.com>
2021-12-08 00:17:21 +0530

    Add files via upload

---
+++ Sparks Bank/footer.php
@@ -0,0 +1,4 @@
+<div id="container">
+    <div id="header">© 2021. Made by ATUL PATIL <br>
+      Project of The Sparks Foundation</div>
+ </div>
---
+++ Sparks Bank/nav.php
@@ -0,0 +1,49 @@
+<!-- nav section starts here -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sparks Bank</title>
+  <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/index.css">
+  <link rel="stylesheet" type="text/css" href="css/table.css">
+  <link rel="stylesheet" type="text/css" href="css/createuser.css">
+  <link rel="stylesheet" type="text/css" href="css/contact.css">
+  <link rel="stylesheet" type="text/css" href="css/aboutme.css">
+  <script type="text/javascript" src="bootstrap/js/bootstrap.bundle.min.js"></script>
+<link rel = "icon" href = "img/logo.png" type = "image/x-icon">
+</head>
+
+
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-yellow" style="background-color: darkgreen;">
+    <div class="container-fluid">
+      
+      <a class="navbar-brand logo" href="index.php">
+      <img src="img/logo.png" alt="logo" height=45 width=45></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+
+
+        <ul class="navbar-nav">
+          <li class="nav-item btn btn-outline-success me-10">
+            <a class="nav-link text-white" href="index.php">Home</a>
+          </li>
+          <li class="nav-item btn btn-outline-success me-2 color: red">
+            <a class="nav-link link-text text-white" href="aboutme.html">About Me</a>
+          </li>
+          <li class="nav-item btn btn-outline-success me-2">
+            <a class="nav-link link-text text-white" href="contactme.php">Contact me</a>
+          </li>
+        </ul>
+
+      </div>
+
+    </div>
+  </nav>
